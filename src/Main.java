871777feccd21b60f4edfa192f1f2e6cc7e17d77
daslater <davidaustinslater@protonmail.com>
2021-09-29 16:04:56 -0500

    Changed addition to subtraction

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a simple addition problem.");
+        System.out.println("This is a simple subtraction problem.");
 
-        int result = 2 + 1;
+        int result = 2 - 1;
 
-        System.out.println("2 + 1 = " + result);
+        System.out.println("2 - 1 = " + result);
 
     }
 
